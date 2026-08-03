a8d8a640eae697c0acb99f0af12c40b43718c26f
brmcdoug <brmcdoug@cisco.com>
2026-08-03 13:58:32 -0700

    clab

--- bridges.sh
+++ bridges.sh
@@ -10,3 +10,12 @@ sudo brctl addbr dc1-host00-l0
 sudo brctl addbr dc1-host00-l1
 sudo brctl addbr dc1-host01-l0
 sudo brctl addbr dc1-host01-l1
+
+sudo ip link set dc0-host00-l0 up
+sudo ip link set dc0-host00-l1 up
+sudo ip link set dc0-host01-l0 up
+sudo ip link set dc0-host01-l1 up
+sudo ip link set dc1-host00-l0 up
+sudo ip link set dc1-host00-l1 up
+sudo ip link set dc1-host01-l0 up
+sudo ip link set dc1-host01-l1 up
